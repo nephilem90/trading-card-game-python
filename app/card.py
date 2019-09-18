e973d4718926bf53550309e7dcbc61942a6a3d02
furt <nephilem90@gmail.com>
2019-09-18 21:42:57 +0200

    inizialize card

--- app/card.py
+++ app/card.py
@@ -1,3 +1,10 @@
 class Card:
-    def __init__(self):
-        return
+    def __init__(self, cost):
+        self.mana = cost
+        self.damage = cost
+
+    def get_mana(self):
+        return self.mana
+
+    def get_damage(self):
+        return self.mana
--- tests/test_card.py
+++ tests/test_card.py
@@ -1,11 +1,14 @@
 import unittest
 from app.card import Card
+import random
 
 
 class TestCard(unittest.TestCase):
     def test_creation(self):
-        card = Card({'mana': 3})
-        self.assertEqual(card.get_mana(), 3)
+        cost = random.randrange(1111, 9999)
+        card = Card(cost)
+        self.assertEqual(card.get_mana(), cost)
+        self.assertEqual(card.get_damage(), cost)
 
 
 if __name__ == '__main__':
