26bd1f00bcb7a608fe5f8ad8ab51341696b61a3b
furt <nephilem90@gmail.com>
2019-10-01 22:58:37 +0200

    test tourn

--- app/game.py
+++ app/game.py
@@ -1,7 +1,15 @@
 class Game:
     def __init__(self, player_one, player_two):
+        self.next = 0
         self.players = [player_one, player_two]
 
     def get_players(self):
         return self.players
 
+    def play_turn(self):
+        self.players[self.next].add_mana(1)
+        if not self.players[self.next].draw_card():
+            self.players[self.next].receive_damage(1)
+        else:
+            self.players[self.next].play_cards()
+        self.next = (self.next + 1) % 2
--- tests/test_game.py
+++ tests/test_game.py
@@ -16,6 +16,29 @@ class GameTest(unittest.TestCase):
         game = Game(player_one, player_two)
         self.assertEqual(game.get_players(), [player_one, player_two])
 
+    def test_player_add_one_mana_in_tourn(self):
+        player_one = MagicMock(name='p1')
+        player_two = MagicMock(name='p2')
+        game = Game(player_one, player_two)
+        game.play_turn()
+        game.get_players()[0].add_mana.assert_called_once_with(1)
+
+    def test_player_get_damage_if_can_not_pick_card(self):
+        player_one = MagicMock(name='p1')
+        player_two = MagicMock(name='p2')
+        player_one.draw_card.return_value = False
+        game = Game(player_one, player_two)
+        game.play_turn()
+        game.get_players()[0].receive_damage.assert_called_once_with(1)
+
+    def test_player_play_card(self):
+        player_one = MagicMock(name='p1')
+        player_two = MagicMock(name='p2')
+        player_one.draw_card.return_value = True
+        game = Game(player_one, player_two)
+        game.play_turn()
+        game.get_players()[0].play_cards.assert_called_once_with()
+
 
 if __name__ == '__main__':
     unittest.main()
