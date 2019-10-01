81737b03da7cf2eac1572b66d222e11005e53d90
furt <nephilem90@gmail.com>
2019-10-01 22:42:34 +0200

    fixed game inizializatin

--- app/game.py
+++ app/game.py
@@ -1,7 +1,5 @@
 class Game:
     def __init__(self, player_one, player_two):
-        player_one.set_life_point(20)
-        player_two.set_life_point(20)
         self.players = [player_one, player_two]
 
     def get_players(self):
